792bc9fb235d4d7a0b64b2a6dc9cfedc7199f762
SpiridovaMaria <lexus754@mail.ru>
2024-12-06 10:59:31 +0300

    tests

---
+++ tests/all tests/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+  import Rectangles from './components/Rectangles.vue'
+  import Twelve_number_test from './components/Twelve_number_test.vue'
+  import Numbers_puzzle from './components/Numbers_puzzle.vue'
+</script> 
+
+<template>
+  <Numbers_puzzle/>
+</template>
